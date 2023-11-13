9e8c52a0309f5248b12539990638af07df0e2fdb
Andre Senna <andre.senna@gmail.com>
2023-11-13 18:17:09 -0300

    Added support classes do make pattern matching
    - Assignment
    - QueryAnswer
    
    Assignment holds a set of mappings label -> value determining a given
    assignment of values to variables. In this initial version, only a simple
    merge() operator were provided but the algebra of Assignments should be
    extended as we port extend the pattern matcher to operates over logical
    expressions (exporession wirth logical operators like AND, OR and NOT).
    
    QueryAnwer is a dataclass to hold a given assignment, which is supposed
    to be a valid answer to a query, and the respective grounded subgraph
    that results from the applying the values given in the assignment to
    the pattern given as query.

--- hyperon_das/utils.py
+++ hyperon_das/utils.py
@@ -1,13 +1,13 @@
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Optional
+from typing import Any, Dict, Optional, Set
+from hyperon_das.exceptions import InvalidAssignment
 
 
 class QueryOutputFormat(int, Enum):
     HANDLE = auto()
     ATOM_INFO = auto()
     JSON = auto()
-
 
 @dataclass
 class QueryParameters:
@@ -17,3 +17,75 @@ class QueryParameters:
     @classmethod
     def values(cls) -> list:
         return list(cls.__dataclass_fields__.keys())
+
+class Assignment:
+
+    def __init__(self):
+        self.labels: Union[Set[str], FrozenSet] = set()
+        self.values: Union[Set[str], FrozenSet] = set()
+        self.mapping: Dict[str, str] = {}
+        self.hashcode: int = 0
+
+    def __hash__(self) -> int:
+        assert self.hashcode
+        return self.hashcode
+
+    def __eq__(self, other) -> bool:
+        assert self.hashcode and other.hashcode
+        return self.hashcode == other.hashcode
+
+    def __lt__(self, other) -> bool:
+        assert self.hashcode and other.hashcode
+        return self.hashcode < other.hashcode
+
+    def __repr__(self) -> str:
+        labels = sorted(self.labels)
+        return str([tuple([label, self.mapping[label]]) for label in sorted(self.labels)])
+
+    def __str__(self) -> str:
+        return self.__repr__()
+
+    def frozen(self):
+        return self.hashcode != 0
+
+    def freeze(self) -> bool:
+        if self.frozen():
+            return False
+        else:
+            self.labels = frozenset(self.labels)
+            self.values = frozenset(self.values)
+            self.hashcode = hash(frozenset(self.mapping.items()))
+            return True
+
+    def assign(
+        self,
+        label: str,
+        value: str,
+        parameters: Optional[Dict[str, Any]] = None
+    ) -> bool:
+        if label is None or value is None or self.frozen():
+            raise InvalidAssignment(
+                message = f"Invalid assignment",
+                details = f"label = {label} value = {value} hashcode = {self.hashcode}"
+            )
+        if label in self.labels:
+            return self.mapping[label] == value
+        else:
+            if parameters and parameters['no_overload'] and value in self.values:
+                return False
+            self.labels.add(label)
+            self.values.add(value)
+            self.mapping[label] = value
+            return True
+
+    def merge(self, other: "Assignment") -> bool:
+        assert not self.frozen()
+        for label, value in other.mapping.items():
+            if not self.assign(label, value):
+                return False
+        return True
+
+@dataclass
+class QueryAnswer:
+    grounded_atom: Optional[Dict] = None
+    assignment: Optional[Assignment] = None
