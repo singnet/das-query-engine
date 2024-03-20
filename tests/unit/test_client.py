cf38bc8f8fab730b63bf1211eac684a001ef3483
levisingularity <rafael.levi@singularitynet.io>
2024-03-20 19:58:27 -0300

    Fix unit tests

--- tests/unit/test_client.py
+++ tests/unit/test_client.py
@@ -6,6 +6,7 @@ from requests import exceptions
 
 from hyperon_das.client import FunctionsClient
 from hyperon_das.exceptions import ConnectionError, RequestError, TimeoutError
+from hyperon_das.utils import deserializer, serializer
 
 
 class TestFunctionsClient:
@@ -15,6 +16,7 @@ class TestFunctionsClient:
             yield mock_request
 
     def test_get_atom_success(self, mock_request):
+        expected_request_data = {"action": "get_atom", "input": {"handle": "123"}}
         expected_response = {
             "handle": "af12f10f9ae2002a1607ba0b47ba8407",
             "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
@@ -23,7 +25,7 @@ class TestFunctionsClient:
         }
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
         result = client.get_atom(handle='123')
@@ -31,12 +33,16 @@ class TestFunctionsClient:
         mock_request.assert_called_with(
             method='POST',
             url='http://example.com',
-            data='{"action": "get_atom", "input": {"handle": "123"}}',
+            data=serializer(expected_request_data),
         )
 
         assert result == expected_response
 
     def test_get_node_success(self, mock_request):
+        expected_request_data = {
+            "action": "get_node",
+            "input": {"node_type": "Concept", "node_name": "human"},
+        }
         expected_response = {
             "handle": "af12f10f9ae2002a1607ba0b47ba8407",
             "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
@@ -45,7 +51,7 @@ class TestFunctionsClient:
         }
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
         result = client.get_node(node_type='Concept', node_name='human')
@@ -53,12 +59,22 @@ class TestFunctionsClient:
         mock_request.assert_called_with(
             method='POST',
             url='http://example.com',
-            data='{"action": "get_node", "input": {"node_type": "Concept", "node_name": "human"}}',
+            data=serializer(expected_request_data),
         )
 
         assert result == expected_response
 
     def test_get_link_success(self, mock_request):
+        expected_request_data = {
+            "action": "get_link",
+            "input": {
+                "link_type": "Similarity",
+                "link_targets": [
+                    "af12f10f9ae2002a1607ba0b47ba8407",
+                    "1cdffc6b0b89ff41d68bec237481d1e1",
+                ],
+            },
+        }
         expected_response = {
             "handle": "bad7472f41a0e7d601ca294eb4607c3a",
             "composite_type_hash": "ed73ea081d170e1d89fc950820ce1cee",
@@ -74,7 +90,7 @@ class TestFunctionsClient:
         }
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
         result = client.get_link(
@@ -85,12 +101,23 @@ class TestFunctionsClient:
         mock_request.assert_called_with(
             method='POST',
             url='http://example.com',
-            data='{"action": "get_link", "input": {"link_type": "Similarity", "link_targets": ["af12f10f9ae2002a1607ba0b47ba8407", "1cdffc6b0b89ff41d68bec237481d1e1"]}}',
+            data=serializer(expected_request_data),
         )
 
         assert result == expected_response
 
     def test_get_links_success(self, mock_request):
+        expected_request_data = {
+            "action": "get_links",
+            "input": {
+                "link_type": "Inheritance",
+                "kwargs": {},
+                "link_targets": [
+                    "4e8e26e3276af8a5c2ac2cc2dc95c6d2",
+                    "80aff30094874e75028033a38ce677bb",
+                ],
+            },
+        }
         expected_response = [
             {
                 "handle": "ee1c03e6d1f104ccd811cfbba018451a",
@@ -101,7 +128,7 @@ class TestFunctionsClient:
         ]
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
         result = client.get_links(
@@ -112,12 +139,26 @@ class TestFunctionsClient:
         mock_request.assert_called_with(
             method='POST',
             url='http://example.com',
-            data='{"action": "get_links", "input": {"link_type": "Inheritance", "kwargs": {}, "link_targets": ["4e8e26e3276af8a5c2ac2cc2dc95c6d2", "80aff30094874e75028033a38ce677bb"]}}',
+            data=serializer(expected_request_data),
         )
 
         assert result == expected_response
 
     def test_query_success(self, mock_request):
+        expected_request_data = {
+            "action": "query",
+            "input": {
+                "query": {
+                    "atom_type": "link",
+                    "type": "Similarity",
+                    "targets": [
+                        {"atom_type": "node", "type": "Concept", "name": "human"},
+                        {"atom_type": "node", "type": "Concept", "name": "monkey"},
+                    ],
+                },
+                "parameters": [],
+            },
+        }
         expected_response = [
             {
                 "handle": "bad7472f41a0e7d601ca294eb4607c3a",
@@ -139,7 +180,7 @@ class TestFunctionsClient:
         ]
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
         query = {
@@ -153,24 +194,34 @@ class TestFunctionsClient:
 
         result = client.query(query, parameters=[])
 
+        mock_request.assert_called_with(
+            method='POST',
+            url='http://example.com',
+            data=serializer(expected_request_data),
+        )
+
         assert result == expected_response
 
     def test_count_atoms_success(self, mock_request):
+        expected_request_data = {"action": "count_atoms", "input": {}}
         expected_response = (14, 26)
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
         result = client.count_atoms()
 
         mock_request.assert_called_once_with(
-            method='POST', url='http://example.com', data='{"action": "count_atoms", "input": {}}'
+            method='POST',
+            url='http://example.com',
+            data=serializer(expected_request_data),
         )
 
-        assert result == tuple(expected_response)
+        assert result == expected_response
 
     def test_send_request_success(self, mock_request):
+        payload = {"action": "get_atom", "input": {"handle": "123"}}
         expected_response = {
             "handle": "af12f10f9ae2002a1607ba0b47ba8407",
             "composite_type_hash": "d99a604c79ce3c2e76a2f43488d5d4c3",
@@ -179,16 +230,15 @@ class TestFunctionsClient:
         }
 
         mock_request.return_value.status_code = 200
-        mock_request.return_value.json.return_value = expected_response
+        mock_request.return_value.content = serializer(expected_response)
 
         client = FunctionsClient(url='http://example.com')
-        payload = {"action": "get_atom", "input": {"handle": "123"}}
         result = client._send_request(payload)
 
         mock_request.assert_called_with(
             method='POST',
             url='http://example.com',
-            data=json.dumps(payload),
+            data=serializer(payload),
         )
 
         assert result == expected_response
