31019b0d79b4f55320923e57abe07ffb780ec47c
marcocapozzoli <marcocapozzoli@gmail.com>
2024-04-08 13:02:38 -0300

    Improve message error when connecting to the server

--- hyperon_das/utils.py
+++ hyperon_das/utils.py
@@ -140,6 +140,8 @@ def connect_to_server(host: str, port: int) -> Tuple[int, str]:
         status_code, message = check_server_connection(uri)
         if status_code == HTTPStatus.OK:
             break
+        elif status_code == HTTPStatus.INTERNAL_SERVER_ERROR:
+            raise Exception(message)
 
     return status_code, uri
 
@@ -149,13 +151,14 @@ def check_server_connection(url: str) -> Tuple[int, str]:
 
     try:
         das_version = get_package_version('hyperon_das')
+        atom_db_version = get_package_version('hyperon_das_atomdb')
 
         with sessions.Session() as session:
             payload = {
                 'action': 'handshake',
                 'input': {
                     'das_version': das_version,
-                    'atomdb_version': get_package_version('hyperon_das_atomdb'),
+                    'atomdb_version': atom_db_version,
                 },
             }
             response = session.request(
@@ -169,18 +172,25 @@ def check_server_connection(url: str) -> Tuple[int, str]:
             try:
                 response = deserialize(response.content)
                 remote_das_version = response.get('das').get('version')
+                remote_atomdb_version = response.get('atom_db').get('version')
             except JSONDecodeError as e:
                 raise Exception(str(e))
             logger().error(
-                f'Package version conflict error when connecting to remote DAS - Local DAS: `{das_version}` - Remote DAS: `{remote_das_version}`'
+                f"Package version conflict error when connecting to remote DAS. Local DAS: 'das: {das_version} - atom_db: {atom_db_version}' -- Remote DAS: 'das: {remote_das_version} - atom_db: {remote_atomdb_version}'"
             )
             raise Exception(
-                f'The version sent by the local DAS is {das_version}, but the expected version on the server is {remote_das_version}'
+                f"The version sent by the local DAS is 'das: {das_version} and atom_db: {atom_db_version}', but the expected version on the server is 'das: {remote_das_version} and atom_db: {remote_atomdb_version}'"
             )
         if response.status_code == HTTPStatus.OK:
             return response.status_code, "Successful connection"
         else:
-            print(f'Response: {deserialize(response.content)}')
-            response.raise_for_status()
-    except (ConnectionError, Timeout, HTTPError, RequestException, Exception) as e:
-        return 400, str(e)
+            try:
+                error_msg = deserialize(response.content).get('error')
+                response.raise_for_status()
+            except pickle.UnpicklingError:
+                raise Exception("A problem occured unpickling an object")
+    except (ConnectionError, Timeout, HTTPError, RequestException) as e:
+        msg = f"{error_msg} - {str(e)}" if error_msg else str(e)
+        return 400, msg
+    except Exception as e:
+        return 500, str(e)
