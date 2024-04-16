a7b8fd6375136e5317ba909a2153f1ad65e9a07f
Andre Senna <andre.senna@gmail.com>
2024-04-16 11:31:33 -0300

    Static analysis fixes

--- hyperon_das/cache/cache_manager.py
+++ hyperon_das/cache/cache_manager.py
@@ -1,8 +1,11 @@
-from hyperon_das.client import FunctionsClient
-from hyperon_das_atomdb.adapters import InMemoryDB, RedisMongoDB
 from typing import Any, Dict, List, Optional, TypeVar, Union
 
+from hyperon_das_atomdb.adapters import InMemoryDB, RedisMongoDB
+
+from hyperon_das.client import FunctionsClient
+
 AdapterDBType = TypeVar("AdapterDBType", RedisMongoDB, InMemoryDB)
+
 
 class CacheManager:
     def __init__(self, cache: AdapterDBType, **kwargs):
