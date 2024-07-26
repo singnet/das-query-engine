d8947d088cf20df56b4538f42a39992586a9d411
Angelo Probst <angelo.probst@gmail.com>
2024-07-26 16:30:24 -0300

    printing msg when python version is lower than the required

--- hyperon_das/__init__.py
+++ hyperon_das/__init__.py
@@ -1,3 +1,9 @@
+import sys
+
+if sys.version_info < (3, 10):
+    print('hyperon_das requires Python 3.10 or higher')
+    sys.exit(1)
+
 from hyperon_das.das import DistributedAtomSpace
 
 __all__ = ['DistributedAtomSpace']
