ee612d4e3eb27c6e83dea886e7c1cd3f391efbe6
Andre Senna <andre.senna@gmail.com>
2024-08-13 10:40:04 -0300

    Added docstrings

--- hyperon_das/cache/cache_controller.py
+++ hyperon_das/cache/cache_controller.py
@@ -6,15 +6,48 @@ from hyperon_das.utils import QueryAnswer
 
 
 class CacheController:
+    """
+    CacheController is the class used by query engines to interface with the caching sub-system.
+
+    Local query engines feed the cache sub-system as queries are processed while remote query
+    engines use it to check for query answers if they are available locally before forwarding
+    these queries to a remote DAS.
+    """
     def __init__(self, system_parameters):
+        """
+        System parameters are allowed to change dinamically after the CacheController
+        object is created. So the behavior of the CacheController regarding each parameter
+        may change accordingly. That's why individual system parameters are not stored individually
+        as object fields. The whole passed SystemParameters object is stored instead and 
+        checked everytime CacheController behavior is supposed to be controlled by some 
+        parameter. For example, the method enabled() will always check for the proper parameter
+        in order to answer if CacheController is enabled or disabled.
+
+        Args:
+            system_parameters (Dict[str, Any], optional): Relevant parameters and their defaults: 
+            {
+                'cache_enabled': False
+            }
+        """
         self.system_parameters = system_parameters
         if self.enabled():
             self.attention_broker = AttentionBrokerGateway(system_parameters)
 
     def enabled(self):
+        """
+        Returns True iff the cache sub-system is enabled (as defined by a system parameter).
+        """
         return self.system_parameters.get("cache_enabled")
 
     def regard_query_answer(self, query_answer: List[QueryAnswer]):
+        """
+        Feed this CacheController with the answers of a query made by an user. These answers are
+        used to feed the cache itself and to allow the AttentionBroker to update its Hebbian
+        Links properly.
+
+        Args:
+            query_answer (List[QueryANswer]): List of QueryAnswer objects.
+        """
         if not self.enabled():
             return
         joint_count = {}
@@ -28,6 +61,15 @@ class CacheController:
         self.attention_broker.stimulate(joint_count)
 
     def add_context(self, context: Context):
+        """
+        Creates a new context to attach importance of atoms.
+
+        Args:
+            context (Context): new COntext object to be added.
+
+        Contexts are used by the AttentionBroker in order to keep different importance values for
+        atoms in different contexts.
+        """
         if not self.enabled():
             return
         for query_answer in context.query_answers:
